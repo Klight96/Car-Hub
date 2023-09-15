c838fa2adc568f204f7a6e7437c9b23bc8f5019a
MACBOOK <macbook@MAKAVELI-MacBook-Air.local>
2023-09-15 13:21:01 +0100

    first  commit

---
+++ components/index.ts
@@ -0,0 +1,10 @@
+import Hero from './Hero'
+import CustomBotton from './CustomBotton'
+import Navbar from './Navbar'
+import Footer from './Footer'
+export {
+    Hero,
+    CustomBotton,
+    Navbar,
+    Footer
+}
